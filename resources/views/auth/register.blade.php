d7961c8ebb8f641a026ebb180e4f7c852b11e314
clement <clementboscher6@gmail.com>
2025-03-19 09:27:49 +0100

    update bio

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
     <input type="text" name="username" placeholder="username">
     <input type="email" name="email" placeholder="Email">
     <input type="password" name="password" placeholder="Password">
+    <input type="textarea" name="bio" placeholder="Bio">
     <button type="submit">Register</button>
  
 </form>  
